64db917e360597f30958611fbd56520ce88058de
Pedro Bacchini <pedro.bacchini@cilia.com.br>
2019-12-02 13:47:30 -0300

    Create Project.

---
+++ src/app/template-form/template-form.module.ts
@@ -0,0 +1,21 @@
+import { HttpModule } from '@angular/http';
+import { CampoControlErroComponent } from './../campo-control-erro/campo-control-erro.component';
+import { FormDebugComponent } from './../form-debug/form-debug.component';
+import { FormsModule } from '@angular/forms';
+import { TemplateFormComponent } from './template-form.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    HttpModule
+  ],
+  declarations: [
+    TemplateFormComponent,
+    FormDebugComponent,
+    CampoControlErroComponent
+  ]
+})
+export class TemplateFormModule { }
