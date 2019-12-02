5042b716ba38ebf92a70e189b01b9c9df34273b6
Pedro Bacchini <pedro.bacchini@cilia.com.br>
2019-12-02 14:02:30 -0300

    2 ways for create reactive forms in angular.

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -10,9 +10,19 @@ export class DataFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
+    /*this.form = new FormGroup({
+      name: new FormControl(null),
+      email: new FormControl(null)
+    });*/
+
+    this.form = this.formBuilder.group({
+      nome : [null],
+      email : [null],
+    });
   }
 
 }
